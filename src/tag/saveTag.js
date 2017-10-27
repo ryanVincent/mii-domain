b5b2e8d9e5fe954a330745a009f0c6102c8c2af2
Ryan Vincent <r.vincent@waracle.com>
2017-10-27 17:35:17 +0100

    save tag

---
+++ src/tag/saveTag.js
@@ -0,0 +1,7 @@
+
+
+const saveTag = (tagRepo) => async (tag, description, isRecurring, isTransfer) => {
+	await tagRepo.createAutoTag(tag, description, isRecurring, isTransfer);
+}
+
+	module.exports = tagTransactions;
